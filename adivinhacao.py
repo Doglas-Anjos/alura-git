c8b6f53d2f74fe59f8674f337b6fc296abcbc83a
doglas anjos <doglas@alunos.utfpr.edu.br>
2020-11-29 12:57:29 -0300

    Criando novo repositorio de controle para jogos

---
+++ adivinhacao.py
@@ -0,0 +1,14 @@
+
+print("************************************","ola bem vido ao jogo de adivinhacao","************************************",sep="\n")
+numero_secreto = 42
+chute = int(input("digite o seu numero\n"))
+print("voce digitou: ",chute)
+interator = 0
+
+if(numero_secreto==chute):
+    print("voce acertou")
+else:
+    if("")
+    print("voce errou")
+
+print("fim do jog")
